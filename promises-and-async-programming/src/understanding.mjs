5a388809405e328a6a8521d8a05f71272a196eaf
TairineEllen <tairine.ellen@gmail.com>
2021-09-15 21:32:11 -0300

    add comments

--- promises-and-async-programming/src/understanding.mjs
+++ promises-and-async-programming/src/understanding.mjs
@@ -1,19 +1,23 @@
 import setText from "./results.mjs";
 
 export function raceCondition() {
-  let xhr = new XMLHttpRequest();
+  /*request to the order statuses endpoint*/
+  let xhr = new XMLHttpRequest(); /*allows us to make requests against an API*/
   let statuses = [];
-  xhr.open("GET", "http://localhost:3000/orderStatuses");
-  xhr.onload = () => {
+  xhr.open("GET", "http://localhost:3000/orderStatuses"); /*specifies the path of the request in the open function*/
+  xhr.onload = () => { /*success*/
     statuses = JSON.parse(xhr.responseText);
   };
 
-  xhr.send();
+  xhr.send(); /*the request is sent*/
 
+  /*request to the orders endpoints*/
   let xhr2 = new XMLHttpRequest();
   xhr2.open("GET", "http://localhost:3000/orders/1");
   xhr2.onload = () => {
-    const order = JSON.parse(xhr2.responseText);
+    /*looks up the order status for that particular order in the list
+    of order statuses that was returned above*/
+    const order = JSON.parse(xhr2.responseText); 
     const description = statuses.map(t => {
       if (t.id === order.orderStatusId) {
         return t.description;
