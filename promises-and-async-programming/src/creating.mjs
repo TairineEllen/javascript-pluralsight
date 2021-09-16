2d518e44e1ec4d25457fa0b13b85640d2b427a23
TairineEllen <tairine.ellen@gmail.com>
2021-09-15 23:43:27 -0300

    create a promise timeout

--- promises-and-async-programming/src/creating.mjs
+++ promises-and-async-programming/src/creating.mjs
@@ -1,6 +1,14 @@
 import setText, { appendText } from "./results.mjs";
 
-export function timeout(){
+export function timeout() {
+  /*promise that will call resolve, which will set the state to fulfilled,
+  and then call our then function*/
+  const wait = new Promise((resolve) => { /*this parameter is the function executor*/
+    setTimeout(() => {
+      resolve('TimeOut!'); /*return value*/
+    }, 1500);
+  }); 
+  wait.then(text => setText(text))
 }
 
 export function interval(){
