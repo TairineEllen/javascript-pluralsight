d076515fc7098315118d87d7180b6b5bb6c6709d
TairineEllen <tairine.ellen@gmail.com>
2021-09-15 22:30:40 -0300

    add an example of a rejected promise

--- promises-and-async-programming/src/consuming.mjs
+++ promises-and-async-programming/src/consuming.mjs
@@ -6,10 +6,15 @@ export function get() {
   axios.get('http://localhost:3000/orders/1')
   .then(({ data }) => {
     setText(JSON.stringify(data));
-  })
+  });
 }
 
 export function getCatch() {
+  axios.get('http://localhost:3000/orders/123')
+  .then(({ data }) => {
+    setText(JSON.stringify(data));
+  })
+  .catch(error => setText(error));
 }
 
 export function chain() {
