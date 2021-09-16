f7ea091dfececb2ac08df2725f14c43b9689a316
TairineEllen <tairine.ellen@gmail.com>
2021-09-16 11:15:56 -0300

    add an example that demonstrates how to use async/await

--- promises-and-async-programming/src/iterating.mjs
+++ promises-and-async-programming/src/iterating.mjs
@@ -1,6 +1,9 @@
 import setText , {appendText} from './results.mjs';
 
-export function get(){
+export async function get() {
+  /*this is making the promise a synchronous call*/
+  const { data } = await axios.get('http://localhost:3000/orders/1');
+  setText(JSON.stringify(data));
 }
 
 export function getCatch(){
