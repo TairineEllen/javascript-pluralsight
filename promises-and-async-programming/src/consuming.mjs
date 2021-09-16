0c68b6717b657acf4a436c7c2377285e8e790e7b
TairineEllen <tairine.ellen@gmail.com>
2021-09-15 22:15:58 -0300

    add an example of a fulfilled promise

--- promises-and-async-programming/src/consuming.mjs
+++ promises-and-async-programming/src/consuming.mjs
@@ -1,6 +1,12 @@
 import setText, {appendText, showWaiting, hideWaiting} from "./results.mjs";
 
+/*Axios returns a result that has a lot of information about the request, such as headers.
+It places the payload in a property named data that contains our requested order.*/
 export function get() {
+  axios.get('http://localhost:3000/orders/1')
+  .then(({ data }) => {
+    setText(JSON.stringify(data));
+  })
 }
 
 export function getCatch() {
