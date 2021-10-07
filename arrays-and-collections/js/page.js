90e152adbfd67b19b1478d681af827a0cb5bb430
TairineEllen <tairine.ellen@gmail.com>
2021-10-07 17:50:34 -0300

    add method to calculate the total yearly sales

--- arrays-and-collections/js/page.js
+++ arrays-and-collections/js/page.js
@@ -2,13 +2,25 @@
 
 let chart = document.getElementById('monthlySales').getContext('2d');
 let pieChart = document.getElementById('deptSales').getContext('2d');
+let yearlyLabel = document.getElementById('yearlyTotal');
 
-let monthlySales = Array.of(12, 19, 3);
+let monthlySales = Array.of(1200, 9000, 3000);
 let monthlyLabels = Array.of('Oct', 'Nov', 'Dec');
 
-let deptSales = Array.of(12, 19, 3);
+let deptSales = Array.of(12, 9, 3);
 let salesLabels = Array.of('Hiking', 'Running', 'Hunting');
 
+function addYearlyTotal(a, b, c) {
+  return a + b + c;
+}
+
+let octNums = Array.of(1200, 1000, 9000);
+let novNums = Array.of(1100, 2000, 9000);
+let decNums = Array.of(4000, 1000, 5000);
+let total = Array.of(addYearlyTotal(...octNums), addYearlyTotal(...novNums), addYearlyTotal(...decNums));
+
+let yearlyTotal = addYearlyTotal(...total);
+yearlyLabel.innerHTML = `$${yearlyTotal}`;
 /*Bar Chart*/
 
 var monthlySalesChart = new Chart(chart, {
