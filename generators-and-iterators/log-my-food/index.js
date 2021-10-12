bbbc4d398e3e16c141c1e62d4a41072b8ae27bfd
TairineEllen <tairine.ellen@gmail.com>
2021-10-12 14:48:38 -0300

    add method to list only the vegan foods

--- generators-and-iterators/log-my-food/index.js
+++ generators-and-iterators/log-my-food/index.js
@@ -1,9 +1,59 @@
+const axios = require('axios');
 const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout,
+  prompt: 'enter command > '
 });
+readline.prompt();
+readline.on('line', line => {
+  switch (line.trim()) {
+    case 'list vegan foods':
+      {
+        axios.get(`http://localhost:3001/food`).then(({ data }) => {
+          let i = 0;
+          const veganOnly = data.filter(food => {
+            return food.dietary_preferences.includes('vegan');
+          })
+          const veganIterable = {
+            [Symbol.iterator]() {
+              return {
+                [Symbol.iterator]() { return this; },
+                next() {
+                  const current = veganOnly[i];
+                  i++;
+                  if (current) {
+                    return { value: current, done: false }
+                  } else {
+                    return { value: current, done: true }
+                  }
+                },
+              };
+            },
+          };
+          for (let val of veganIterable) {
+            console.log(val.name);
+          }
+          readline.prompt()
+        });
+      }
+      break;
+    case 'log':
+      {
+        readline.question('What would you like to log today? ', async item => {
+          const { data } = await axios.get('http://localhost:3001/food');
+          const it = data[Symbol.iterator]();
+          let position = it.next();
+          while (!position.done) {
+            const food = position.value.name;
+            if (food === item) {
+              console.log(`${item} has ${position.value.calories} calories`);
+            }
+            position = it.next();
+          }
 
-readline.question('What would you like to log today? ', item => {
-  console.log(item);
-  readline.close();
+          readline.prompt();
+        })
+      }
+  }
 })
+
